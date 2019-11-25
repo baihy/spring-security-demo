034875b67c53936c02a04cd851ebc12157f0d967
huayang.bai <1503091115@qq.com>
2019-11-25 18:01:56 +0800

    初始化导入

---
+++ distributed-security/src/test/java/com/baihy/distributedsecurityuaa/DistributedSecurityUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baihy.distributedsecurityuaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSecurityUaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
